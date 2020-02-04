21ff98673bf452395955477599caa98934574a1c
Adrian Mohabir <mohabiradrian@gmail.com>
2020-02-04 17:43:37 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+    attr_accessor :name, :type, :booty_attributes
+
+    @@all = []
+
+    def initialize(name, type, booty_attributes)
+        @name = name
+        @type = type
+        @booty_attributes = booty_attributes
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.clear
+        Ship.clear
+    end 
+
+
 end
